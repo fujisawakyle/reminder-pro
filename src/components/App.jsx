e247db72214a5dc7e3451cec2ae0d6217c78864f
fujisawakyle <fujisawakyle@gmail.com>
2017-10-01 20:45:15 -0700

    add cookies storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addReminder, deleteReminder } from '../actions';
+import moment from 'moment';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state= {
-            text: ''
+            text: '',
+            dueDate: ''
         }
     }
 
     addReminder() {    
-        this.props.addReminder(this.state.text);
+        this.props.addReminder(this.state.text, this.state.dueDate);
     }
 
     deleteReminder(id) {
-        console.log('deleting in app', id);
-        console.log('this.props', this.props);
         this.props.deleteReminder(id);
     }
 
@@ -28,7 +28,10 @@ class App extends Component {
                     reminders.map(reminder => {
                         return (
                             <li key={reminder.id} className="list-group-item">
-                                <div className="list-item">{reminder.text}</div>
+                                <div className="list-item">
+                                    <div>{reminder.text}</div>
+                                    <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+                                </div>
                                 <div 
                                     className='list-item delete-button'
                                     onClick={() => this.deleteReminder(reminder.id)}
@@ -56,6 +59,11 @@ class App extends Component {
                                 className="form-control"
                                 placeholder="I have to..."
                                 onChange={event => this.setState({text: event.target.value})}
+                            />
+                            <input 
+                                className="form-control"
+                                type="datetime-local"
+                                onChange={event => this.setState({dueDate: event.target.value})}
                             />
                         </div>
                         
